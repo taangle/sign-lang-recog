9bc1c5b46565ff2a485add5a64c420fb07188305
Trevor Angle <trevoraangle@gmail.com>
2018-04-13 19:33:12 -0700

    Finished network

--- ImagePrepro.py
+++ ImagePrepro.py
@@ -4,17 +4,21 @@ from keras.layers import *
 from keras.preprocessing.image import ImageDataGenerator
 
 model = Sequential()
-model.add(Conv2D(24, (3, 3), input_shape=(50, 50, 1), batch_size=24))
+model.add(Conv2D(24, (3, 3), input_shape=(1, 50, 50), batch_size=24))
 model.compile(optimizer="sgd", loss="categorical_crossentropy")
 
-data_gen = ImageDataGenerator(rotation_range=15,
-                              width_shift_range=15,
-                              height_shift_range=15)
+data_gen = ImageDataGenerator(
+                            rotation_range=10,
+                              width_shift_range=.1,
+                              height_shift_range=.1,
+                              shear_range=.02,
+                              zoom_range=.05
+                              )
 
-data_flow = data_gen.flow_from_directory(directory='D:\\Trevor\\Documents\\_Code\\Python\\Machine Learning\\Sign Language\\data\\signingsavvy\\letters',
-                                         save_to_dir='D:\\Trevor\\Documents\\_Code\\Python\\Machine Learning\\Sign Language\\data\\signingsavvy\\trial_2',
-                                         target_size=(100, 100),
-                                         batch_size=24,
+data_flow = data_gen.flow_from_directory(directory='D:\\Trevor\\Documents\\_Code\\Python\\Machine Learning\\Sign Language\\SignLanguageProject\\data\\classified\\train',
+                                         save_to_dir='D:\\Trevor\\Documents\\_Code\\Python\\Machine Learning\\Sign Language\\SignLanguageProject\\data\\dekart\\trial_gen',
+                                         target_size=(150, 150),
+                                         batch_size=60,
                                          shuffle=False,
                                          seed=19,
                                          color_mode='grayscale',
--- sign_lang_recog_convnet.py
+++ sign_lang_recog_convnet.py
@@ -10,18 +10,91 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 
-img_width = 100
-img_height = 100
+img_width = 150
+img_height = 150
 train_data_directory = 'data/classified/train'
-validation_data_directory = ''
-data_size = 0
-validation_size = 0
-batch_size = 16
+validation_data_directory = 'data/classified/validation'
+data_size = 338
+validation_size = 24
+batch_size = 24
 data_steps_per_epoch = data_size / batch_size
 validation_steps_per_epoch = validation_size / batch_size
-epochs = 50
+epochs = 4
 
 if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
+    input_shape = (1, img_width, img_height)
 else:
-    input_shape = (img_width, img_height, 3)
+    input_shape = (img_width, img_height, 1)
+
+model = Sequential()
+
+# input layer
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# first hidden
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# second hidden
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# third hidden
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(.01))
+
+# output layer
+model.add(Dense(24))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# augmentation config
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=10,
+        width_shift_range=.1,
+        height_shift_range=.1,
+        shear_range=.02,
+        zoom_range=.05
+)
+
+# test config
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# train generator
+train_generator = train_datagen.flow_from_directory(
+    train_data_directory,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    color_mode='grayscale',
+    shuffle=True
+)
+
+# validation generator
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_directory,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    color_mode='grayscale'
+)
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=data_steps_per_epoch,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_steps_per_epoch
+)
+
+model.save_weights('try_2.h5')
+
+
